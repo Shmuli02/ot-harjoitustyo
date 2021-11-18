949fad5b9bc2d15e6c8026ba439197f43ac44974
Shmuli02 <samuli.kaleva@gmail.com>
2021-11-18 12:52:07 +0000

    repositories in index

--- projekti/src/index.py
+++ projekti/src/index.py
@@ -1,8 +1,15 @@
 from menu import Menu
+from ui.ui import UI
+from database_connection import get_database_connection
+from repository.house_repository import HouseRepository
+from repository.transaction_repository import TransactionsRepository
 
 def main():
-    menu = Menu()
-    menu.commandLineRunner()
+    ui = UI()
+    house_repository = HouseRepository(get_database_connection())
+    transaction_repository = TransactionsRepository(get_database_connection())
+    menu = Menu(ui,house_repository,transaction_repository)
+    menu.command_line_runner()
 
 
 if __name__ == '__main__':
