47462deab9458694de5ab70954c8440e8d9adda7
Shmuli02 <samuli.kaleva@gmail.com>
2021-11-18 10:12:39 +0000

    Add transactions class

---
+++ projekti/src/transaction.py
@@ -0,0 +1,9 @@
+class Transaction:
+    def __init__(self,house_id,category_id,amount,description,transaction_type):
+        self.house_id = house_id
+        self.category = category_id
+        self.amount = amount
+        self.description = description
+        self.transaction_type = transaction_type
+        
+
