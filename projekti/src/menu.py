a896e3834304bbb08fe0e63bdf5c3cd9e4133a35
Shmuli02 <samuli.kaleva@gmail.com>
2021-11-18 10:11:56 +0000

    menu handle new transactions

--- projekti/src/menu.py
+++ projekti/src/menu.py
@@ -1,57 +1,104 @@
-from house import House
+from database_connection import get_database_connection
+from repository.house_repository import HouseRepository
+from repository.transaction_repository import TransactionsRepository
 
 class Menu:
-    def __init__(self,houses=[]):
+    def __init__(self):
         self.commands = [
             {'command':'1','description':'Raportti'},
             {'command':'2','description':'Asunnot'},
             {'command':'3','description':'Luo uusi asunto'},
             {'command':'4','description':'Lisää/muokkaa asunnon tuloja ja menoja'},
+            {'command':'5','description':'Muokkaa asunnon tietoja'},
             {'command':'X','description':'Poistu'},
         ]
-        self.houses = houses
+        self._house_repository = HouseRepository(get_database_connection())
+        self._transaction_repository = TransactionsRepository(get_database_connection())
+        self.houses = self._house_repository.get_houses()
         
-    def printCommands(self):
+    def print_commands(self):
         print('Saatavilla olevat ominaisuudet:')
         for command in self.commands:
-            print(f"{command['command']}  {command['description']}")
+            print(f"{command['command']} {command['description']}")
+    
+    def setup_houses(self):
+        self.houses = self._house_repository.get_houses()
 
-    def commandLineRunner(self):
+    def command_line_runner(self):
         
-        self.printCommands()
+        self.print_commands()
         
         while True:
             command = input('komento: ')
 
             if command == '1':
-                self.printReport()
+                self.print_report()
             elif command == '2':
-                self.printHouses()
+                self.print_houses()
             elif command == '3':
-                self.newHouse()
+                self.new_house()
             elif command == '4':
-                self.addEditIncomesExpenses()
+                self.add_edit_transactions()
+            elif command == '5':
+                self.update_house_info()
+            elif command == 'help':
+                self.print_commands()
             elif command == 'X':
                 break
             else:
                 print('Väärä syöte')
     
-    def printReport(self):
+    def print_report(self):
         pass
 
-    def printHouses(self):
+    def print_houses(self):
         if len(self.houses) == 0:
             print('ei asuntoja')
         else:
             for house in self.houses:
-                print(f"{house.name}  {house.address}")
+                print(f"{house.id} {house.name}  {house.address}")
 
-    def newHouse(self):
-        newHouseName = input('Asunnon nimi: ')
-        newHouseAddress = input('Osoite: ')
-        newHouse = House(newHouseName,newHouseAddress)
-        self.houses.append(newHouse)
-    
-    def addEditIncomesExpenses(self):
+    def new_house(self):
+        new_house_name = input('Asunnon nimi: ')
+        new_house_address = input('Osoite: ')
+        self._house_repository.create_house(new_house_name,new_house_address)
+        self.setup_houses()
+
+    def update_house_info(self):
         pass
+
     
+    def add_edit_transactions(self):
+        
+        add_edit_commands = [
+            {'command':'1','description':'Uusi tulo'},
+            {'command':'2','description':'Uusi meno'},
+            {'command':'3','description':'muokkaa tuloa'},
+            {'command':'4','description':'muokkaa menoa'},
+            {'command':'X','description':'Palaa päävalikkoon'}
+
+        ]
+        
+        while True:
+            command = input('Komento: ')
+
+            if command == '1':
+                house_id = input('Talo id: ')
+                category_id = input('Kategoria: ')
+                amount = input('Summa: ')
+                description = input('Selite: ')
+                new_transaction = self._transaction_repository.add_income(house_id,category_id,amount,description)
+            elif command == '2':
+                house_id = input('Talo id: ')
+                category_id = input('Kategoria: ')
+                amount = input('Summa: ')
+                description = input('Selite: ')
+                new_transaction = self._transaction_repository.add_expense(house_id,category_id,amount,description)
+            elif command == '3':
+                pass
+            elif command == '4':
+                pass
+            elif command == 'X':
+                break
+            else:
+                print('Väärä syöte')
