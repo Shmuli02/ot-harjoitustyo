12d7a5c53fac49747f06a7f80fc81cd4d4e6a588
Shmuli02 <samuli.kaleva@gmail.com>
2021-11-22 10:20:18 +0000

    House add income/expense function

--- projekti/src/house.py
+++ projekti/src/house.py
@@ -1,4 +1,5 @@
 from repository.transaction_repository import TransactionsRepository
+from database_connection import get_database_connection
 
 class House:
     def __init__(self,id,houseName,address,expenses=[],incomes=[]):
@@ -7,14 +8,24 @@ class House:
         self.address = address
         self.expenses = expenses
         self.incomes = incomes
+        self._transaction_repository = TransactionsRepository(get_database_connection())
 
-    def add_expense(self,amount,category,description):
-        if amount>0:
-            self.expenses.append({'amount':amount,'category':category,'description':description})
+    
 
-    def add_income(self,amount,category,description):
-        if amount>0:
-            self.incomes.append({'amount':amount,'category':category,'description':description})
+
+    def add_income(self,category_id,amount,description):
+        if int(amount)>0:
+            new_transaction = self._transaction_repository.add_income(self.id,category_id,amount,description)
+            return True
+        else:
+            return False
+    
+    def add_expense(self,category_id,amount,description):
+        if int(amount)>0:
+            new_transaction = self._transaction_repository.add_expense(self.id,category_id,amount,description)
+            return True
+        else:
+            return False
 
     def edit_expense(self):
         pass
@@ -25,6 +36,5 @@ class House:
     def house_report(self):
         incomes = sum([i.amount for i in self.incomes])
         expenses = sum([i.amount for i in self.expenses])
-        profit = incomes-expenses
-        
-        return {'incomes':incomes,'expenses':expenses,'profit':profit}
+        report_values = {'incomes':incomes,'expenses':expenses}
+        return report_values
